d8f97bbe553ba0d5c3b00a94fcc64bc82e3bc973
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-16 13:31:54 +0530

    project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import config from './config/config';
+
+function App() {
+  console.log(config.appwriteEndpointURL);
+  
+
+  return (
+    <>
+   <h1 className='text-3xl text-amber-400'>Write-Free</h1>
+    </>
+  )
+}
+
+export default App
